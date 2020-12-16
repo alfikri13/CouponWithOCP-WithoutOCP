4a651e845dcd0e8c9f83ab9d8625c8b9926f020c
al fikri utomo <61920101+alfikri13@users.noreply.github.com>
2020-12-16 17:07:14 +0700

    Create CouponWithMaxNominal.cs

---
+++ CouponWithOCP/CouponWithOCP/CouponWithOCP/CouponWithMaxNominal.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    class CouponWithMaxNominal : Coupon
+    {
+        public double discPercentage = 0;
+        public double discNominal = 0;
+        private double v;
+
+        public CouponWithMaxNominal(double v)
+        {
+            this.v = v;
+        }
+
+        public CouponWithMaxNominal(double discPercentage, double discNominal)
+        {
+            this.discPercentage = discPercentage;
+            this.discNominal = discNominal;
+        }
+
+        public override double calculate(double originPrice)
+        {
+            double discPercentageInRupiah = discPercentage * originPrice / 100;
+            double finalPrice = 0;
+            if (discPercentageInRupiah > discNominal)
+            {
+                finalPrice = originPrice - discNominal;
+            }
+            else
+            {
+                finalPrice = originPrice - discPercentageInRupiah;
+            }
+            return finalPrice;
+        }
+    }
+}
